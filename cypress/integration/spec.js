c185242f3fdd30715d16fc44e393e092c30c4d58
Stan Fisher <sfisher@cleo.com>
2018-11-23 12:50:26 -0600

    reproducing issue in cypress

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,33 @@
+const getLeftNavMenuItem = (name) => cy.get('.navbar.navbar-inverse ul#left-nav>li').contains(name);
+const getMainNavMenuItem = (name) => cy.get('.navbar.navbar-inverse ul#main-nav>li').contains(name);
+
 describe('page', () => {
-  it('works', () => {})
-})
+  before(() => {
+    cy.visit('/');
+
+    getLeftNavMenuItem('Features').click();
+    cy.url().should('contain', '/features/');
+
+    getLeftNavMenuItem('Pricing').click();
+    cy.url().should('contain', '/pricing/');
+  });
+
+  it('works', () => {
+    cy.wrap(true).should('be.true');
+  });
+
+  // ***** NOTE *****
+  // The presence of this failing tests means that the clicks in the 'after' block don't work.
+  // If this test is either removed or changed to be passing, then the 'after' block will work.
+  it('fails', () => {
+    cy.wrap(false).should('be.true');
+  })
+
+  after(() => {
+    getMainNavMenuItem('Blog').click();
+    cy.url().should('contain', '/blog/');
+
+    getMainNavMenuItem('Support').click();
+    cy.url().should('contain', '/support/');
+  });
+});
